c738e0d3eeceaa725fd885415f3e260daff5f14e
katherine-kania <katherine.kania@gmail.com>
2022-04-26 12:11:04 -0400

    header auth added to flowers index

--- src/api/flowers.js
+++ src/api/flowers.js
@@ -1,7 +1,15 @@
 import apiUrl from '../apiConfig'
 import axios from 'axios'
 
+
 // index function
-export const getAllFlowers = () => {
-    return axios(`${apiUrl}/flowers`)
+export const getAllFlowers = (user) => {
+    console.log('this is the user:', user)
+    return axios({
+        url: `${apiUrl}/flowers/`,
+        method: 'GET',
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+    })
 }
